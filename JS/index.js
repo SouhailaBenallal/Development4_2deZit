9f2bf36e593b84c05568eff02225800f51478dd8
Souhaila Ben allal <souhailabenallal@MBP-de-Souhaila.lan>
2020-08-28 21:48:41 +0200

    Search

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,33 @@
 let data = [];
+let genders = [];
+let categories = [];
+let filteredData = [];
+let isFilterOn = false;
+
+function onSearch(event) {
+  event.preventDefault();
+  const searchInput = document.getElementById("search-input");
+  const searchValue = searchInput.value;
+
+  if (searchValue) {
+    const matchedValues = data.filter((el) => {
+      const elName = el.name.toLowerCase().trim();
+      const SearchValueToCompare = searchValue.toLowerCase().trim();
+
+      return elName.includes(SearchValueToCompare);
+    });
+    isFilterOn = true;
+    hydrateFilteredData(matchedValues);
+  } else {
+    isFilterOn = false;
+  }
+  displayData();
+}
+
+function attachEvents() {
+  const searchBtn = document.getElementById("search-btn");
+  searchBtn.addEventListener("click", onSearch);
+}
 
 async function fetchData() {
   const res = await fetch("../data.json");
@@ -10,64 +39,32 @@ async function fetchData() {
 function displayData() {
   const list = document.getElementById("list");
 
-  data.forEach((el) => {
-    const card = createCard(el);
-    list.appendChild(card);
-  });
+  // Empty the list content
+  list.innerHTML = "";
+
+  // Fill list content
+  if (isFilterOn) {
+    filteredData.forEach((el) => {
+      const card = createCard(el);
+      list.appendChild(card);
+    });
+  } else {
+    data.forEach((el) => {
+      const card = createCard(el);
+      list.appendChild(card);
+    });
+  }
 }
 
 function hydrateData(newData) {
-  const copyData = [...newData];
-  data = copyData;
+  data = [...newData];
 }
-
-function createCard(d) {
-  // Create card & add class
-  const card = document.createElement("LI");
-  card.classList.add("card");
-
-  // Link
-  const link = document.createElement("A");
-  link.href = d.slug;
-
-  // Add link to card
-  card.appendChild(link);
-
-  const divHeader = document.createElement("DIV");
-  divHeader.classList.add("header");
-  const divBody = document.createElement("DIV");
-  divBody.classList.add("body");
-
-  link.appendChild(divHeader);
-  link.appendChild(divBody);
-
-  if (d.genre) {
-    const tag = document.createElement("SPAN");
-    tag.classList.add("tag");
-    tag.innerText = d.genre;
-    divHeader.appendChild(tag);
-  }
-
-  const play = document.createElement("div");
-  play.classList.add("play-btn");
-
-  divHeader.appendChild(play);
-
-  const title = document.createElement("h3");
-  title.innerText = d.name;
-  const desc = document.createElement("p");
-  desc.innerText = d.excerpt;
-  const time = document.createElement("div");
-  time.innerText = "100 min";
-
-  divBody.appendChild(title);
-  divBody.appendChild(desc);
-  divBody.appendChild(time);
-
-  return card;
+function hydrateFilteredData(newData) {
+  filteredData = [...newData];
 }
 
 (async function () {
+  attachEvents();
   const dataFetched = await fetchData();
   hydrateData(dataFetched);
   displayData();
